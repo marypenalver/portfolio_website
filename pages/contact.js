5718e2f4cfee8b3fe9994f1417ca80f869c0cd64
mariapenalver <marypenalverop@gmail.com>
2025-12-13 19:21:44 -0500

    contact page text glitch

--- pages/contact.js
+++ pages/contact.js
@@ -75,8 +75,8 @@ export default function Contact() {
     <p className="hidden md:block absolute text-center bottom-10 font-dropline text-black text-xs mt-10">
             INSTAGRAM: MARIAPENALVERR
     </p>
-        <p className="hidden md:block absolute left-20 bottom-10 font-dropline text-xs mt-2">EMAIL: CONTACT.MARIAPENALVER@GMAIL.COM</p>
-        <p className="hidden md:block absolute right-[10%] bottom-10 font-dropline text-xs mt-2">PRODUCTION. PHOTOGRAPHY. WEB DEVELOPMENT</p>
+        <p className="hidden md:block absolute right-[42%] bottom-10 font-dropline text-xs mt-10">EMAIL: CONTACT.MARIAPENALVER@GMAIL.COM</p>
+        <p className="hidden md:block absolute right-[10%] bottom-10 font-dropline text-xs mt-10">PRODUCTION. PHOTOGRAPHY. WEB DEVELOPMENT</p>
       {/* mobile only */}
       <div className="mt-6 flex flex-col gap-2 text-center text-xs font-dropline md:hidden">
         <p>INSTAGRAM: MARIAPENALVERR</p>
